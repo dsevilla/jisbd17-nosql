9ef8fff467f41c16351b83b3c1301ca6e5ba3d76
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2017-07-17 22:40:19 +0200

    hbase

--- utils/functions.py
+++ utils/functions.py
@@ -111,3 +111,17 @@ def say_(string, font):
               string, (255,255,255,1), font=font)
 
     return image
+
+# http://stackoverflow.com/a/30525061/62365
+class DictTable(dict):
+    # Overridden dict class which takes a dict in the form {'a': 2, 'b': 3},
+    # and renders an HTML Table in IPython Notebook.
+    def _repr_html_(self):
+        html = ["<table width=100%>"]
+        for key, value in self.iteritems():
+            html.append("<tr>")
+            html.append("<td>{0}</td>".format(key))
+            html.append("<td>{0}</td>".format(value))
+            html.append("</tr>")
+        html.append("</table>")
+        return ''.join(html)
